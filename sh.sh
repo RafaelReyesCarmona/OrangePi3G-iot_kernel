0e03829e44066b5dc379cb7813277fe45c2c36bf
Rafael Reyes <rafael.reyes.carmona@gmail.com>
2023-08-29 19:01:34 +0200

    Update

--- sh.sh
+++ sh.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
-TOOLCHAIN=/xspace/OrangePi/MTK/SourceCode/OrangePi_3G-IOT/toolchain/arm-eabi-4.7/bin/arm-eabi-
-OUTPUT=/xspace/OrangePi/MTK/SourceCode/OrangePi_3G-IOT/output/obj/KERNEL_OBJ
-export PATH=/xspace/OrangePi/MTK/SourceCode/OrangePi_3G-IOT/external/make:$PATH
-make CROSS_COMPILE=$TOOLCHAIN TARGET_PRODUCT=hexing72_cwet_kk silentoldconfig O=$OUTPUT
-#make CROSS_COMPILE=$TOOLCHAIN TARGET_PRODUCT=hexing72_cwet_lca 3g-iot-A_defconfig O=$OUTPUT
-make $1 CROSS_COMPILE=$TOOLCHAIN TARGET_PRODUCT=hexing72_cwet_kk O=$OUTPUT
+TOOLCHAIN=/usr/bin/arm-none-eabi-
+#TOOLCHAIN=/xspace/OrangePi/MTK/SourceCode/OrangePi_3G-IOT/toolchain/arm-eabi-4.7/bin/arm-eabi-
+
+OUTPUT=../output/obj/KERNEL_OBJ
+export PATH=../external/make:$PATH
+#make CROSS_COMPILE=$TOOLCHAIN TARGET_PRODUCT=hexing72_cwet_kk silentoldconfig O=$OUTPUT
+make CROSS_COMPILE=$TOOLCHAIN TARGET_PRODUCT=hexing72_cwet_lca 3g-iot-A_linux_defconfig O=$OUTPUT
+#make $1 CROSS_COMPILE=$TOOLCHAIN TARGET_PRODUCT=hexing72_cwet_kk O=$OUTPUT
 
 #MTK_PROJECT=hexing72_cwet_lca
 #kernel_zimg="../output/obj/KERNEL_OBJ/arch/arm/boot/zImage"
